7d98c11c591971caf2d5d0971da8c7731b404b0a
Rudys Natanael Acosta <natanael926@gmail.com>
2016-11-22 17:16:03 -0400

    with en serie fibonacci

---
+++ inmension/plural2.py
@@ -0,0 +1,19 @@
+import re
+
+def build_match_and_apply_functions(pattern, search, replace):
+    def matches_rule(word):
+        return re.search(pattern, word)
+    def apply_rule(word):
+        return re.sub(search, replace, word)
+    return (matches_rule, apply_rule)
+
+patterns =\
+    (
+        ('[sxz]$',           '$', 'es')
+        ('[^aeioudgkprt]h$', '$', 'es'),
+        ('(qu|[^aeiou])y$',  'y$', 'ies'),
+        ('$',                '$', 's')
+    )
+
+rules = [build_match_and_apply_functions(pattern, search, replace)
+         for (pattern, search, replace) in patterns]
